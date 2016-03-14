20115659ddb7a982273ba8be8d55887e9401f1a9
emad21 <mohd.emad@y7mail.com>
2016-03-14 04:56:22 -0700

    Implementing minimax algorithm for predicting computer position

--- app/src/main/java/com/example/emad/myapplication/TicTacToe.java
+++ app/src/main/java/com/example/emad/myapplication/TicTacToe.java
@@ -50,5 +50,79 @@ public class TicTacToe {
             }
             return availableTiles;
         }
+
+
+
+
+            public int checkforwinner(){
+            // CHECK FOR ROWS
+            for (int i=0;i<=6 ;i+=3 ) {
+                if (mBoard[i] == H_Player &&
+                        mBoard[i+1] == H_Player &&
+                        mBoard[i+2] == H_Player ) {
+                    return 2;
+                }
+                if (mBoard[i] == A_Player &&
+                        mBoard[i+1] == A_Player &&
+                        mBoard[i+2] == A_Player ) {
+                    return 3;
+                }
+
+            }
+
+            // CHECK FOR columns
+            for (int i=0;i<=2 ;i++ ) {
+                if (mBoard[i] == H_Player &&
+                        mBoard[i+3] == H_Player &&
+                        mBoard[i+6] == H_Player ) {
+                    return 2;
+                }
+                if (mBoard[i] == A_Player &&
+                        mBoard[i+3] == A_Player &&
+                        mBoard[i+6] == A_Player) {
+                    return 3;
+                }
+
+            }
+            // for diagonals
+            //right diagonal
+             for (int i=0;; ) {
+                if (mBoard[i] == H_Player &&
+                        mBoard[i+4] == H_Player &&
+                        mBoard[i+8] == H_Player ) {
+                    return 2;
+                }
+                if (mBoard[i] == A_Player &&
+                        mBoard[i+4] == A_Player &&
+                        mBoard[i+8] == A_Player) {
+                    return 3;
+                }
+
+            }
+             //left diagonal
+             for (int i=2;; ) {
+                if (mBoard[i] == H_Player &&
+                        mBoard[i+2] == H_Player &&
+                        mBoard[i+2] == H_Player ) {
+                    return 2;
+                }
+                if (mBoard[i] == A_Player &&
+                        mBoard[i+2] == A_Player &&
+                        mBoard[i+2] == A_Player) {
+                    return 3;
+                }
+
+            }
+           
+            // for tie
+            for (int i=0;i< getBoard_size();i++){
+
+                if(mBoard[i] != H_Player && mBoard[i] != A_Player && mBoard[i] != Empty_space){
+                    return  1;
+                }
+            }
+
+            return 0;
+        }
         
 }
